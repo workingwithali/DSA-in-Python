c9d74587dd707c8aec171681d42837abc98b2ef2
workingwithali <alirashid2020e@gmail.com>
2024-06-20 15:02:42 +0500

    variable and datatype

---
+++ Basics of Python/Variables and Data Types/DataTaype.py
@@ -0,0 +1,56 @@
+# Numeric Types:
+
+# int: Integer, e.g., x = 10
+age = 30
+print(type(age))  # Output: <class 'int'>
+
+# float: Floating-point number, e.g., pi = 3.14
+pi = 3.14159
+print(type(pi))  # Output: <class 'float'>
+
+# complex: Complex number, e.g., z = 1 + 2j
+z = 1 + 2j
+print(type(z))
+
+# Sequence Types:
+
+# str: String, e.g., name = "Alice"
+greeting = "Hello, World!"
+print(type(greeting))  # Output: <class 'str'>
+
+# list: List, e.g., numbers = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "cherry"]
+print(type(fruits))  # Output: <class 'list'>
+
+# tuple: Tuple, e.g., coordinates = (10.0, 20.0)
+coordinates = (10.0, 20.0)
+print(type(coordinates))  # Output: <class 'tuple'>
+
+# Mapping Type:
+
+# dict: Dictionary, e.g., student = {"name": "Alice", "age": 25}
+person = {"name": "Alice", "age": 30}
+print(type(person))  # Output: <class 'dict'>
+
+#  Set Types:
+
+# set: Set, e.g., unique_numbers = {1, 2, 3, 4, 5}
+unique_numbers = {1, 2, 3, 4, 5}
+print(type(unique_numbers))  # Output: <class 'set'>
+
+# frozenset: Immutable set, e.g., immutable_set = frozenset([1, 2, 3, 4, 5])
+immutable_set = frozenset([1, 2, 3, 4, 5])
+print(type(immutable_set))
+
+# Boolean Type:
+
+# bool: Boolean, e.g., is_active = True
+is_active = True
+print(type(is_active))  # Output: <class 'bool'>
+
+# None Type:
+
+# None: Represents the absence of a value, e.g., value = None
+
+value = None
+print(type(value))  # Output: <class 'NoneType'>
---
+++ Basics of Python/Variables and Data Types/Variable.py
@@ -0,0 +1,5 @@
+# A variable in Python is created the moment you first assign a value to it. Python variables do not need explicit declaration to reserve memory space. The declaration happens automatically when you assign a value to a variable. The equal sign = is used to assign values to variables.
+
+
+x = 5
+y = "Hello, World!"
